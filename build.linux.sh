4e2072c5c9958e940825a8f60b7e75f738042d3c
rinukkusu <rinukkusu@sub-r.de>
2019-02-05 19:03:02 +0100

    add thinner output without the full framework attached

--- build.linux.sh
+++ build.linux.sh
@@ -2,3 +2,5 @@
 
 dotnet publish --self-contained -c Release -r linux-x64 -o ../build/linux-x64/Speedtest Speedtest/Speedtest.csproj
 dotnet publish --self-contained -c Release -r linux-x64 -o ../build/linux-x64/Speedtest.InfluxDb Speedtest.InfluxDb/Speedtest.InfluxDb.csproj
+dotnet build -c Release -r linux-x64 -o ../build/linux-x64-thin/Speedtest Speedtest/Speedtest.csproj
+dotnet build -c Release -r linux-x64 -o ../build/linux-x64-thin/Speedtest.InfluxDb Speedtest.InfluxDb/Speedtest.InfluxDb.csproj
--- build.windows.bat
+++ build.windows.bat
@@ -1,2 +1,4 @@
 dotnet publish --self-contained -c Release -r win-x64 -o ../build/win-x64/Speedtest Speedtest/Speedtest.csproj
 dotnet publish --self-contained -c Release -r win-x64 -o ../build/win-x64/Speedtest.InfluxDb Speedtest.InfluxDb/Speedtest.InfluxDb.csproj
+dotnet build -c Release -r win-x64 -o ../build/win-x64-thin/Speedtest Speedtest/Speedtest.csproj
+dotnet build -c Release -r win-x64 -o ../build/win-x64-thin/Speedtest.InfluxDb Speedtest.InfluxDb/Speedtest.InfluxDb.csproj
